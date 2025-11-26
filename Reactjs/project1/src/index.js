28b8d93554ed75fdea6e6b88915f0326f36dce28
azartechnet <azartechnet@gmail.com>
2025-11-26 19:48:30 +0530

    Update index.js

--- Reactjs/project1/src/index.js
+++ Reactjs/project1/src/index.js
@@ -1,11 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App=()=>{
+/*const App=()=>{
   return(
     <h1>Welcome to the Application</h1>
   )
 }
 
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+r1.render(<App/>)*/
+
+//Dynamic Rendering
+
+/*const Sample=()=>{
+  const name="azar";
+  const age=30;
+  return(
+    <div>
+      <h1>Hello,{name}</h1>
+      <p>Your age is::{age}</p>
+      <p>Todays date is::{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//React without jsx
+
+/*const myelem1=React.createElement("h1",null,"welcome")
+const myelem2=React.createElement("p",null,"This is a Paragraph")
+const r1=ReactDOM.createRoot(document.getElementById("root"))
+r1.render(React.createElement("div",null,myelem1,myelem2))*/
+
+//Login Form
+
+const Login=()=>{
+  return(
+    <>
+      <form>
+        <label>UserName</label>
+        <input type="text"/>
+        <label>Password</label>
+        <input type="password"/>
+        <input type="submit" value="Login"/>
+      </form>
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById("root"))
+r1.render(<Login/>)
