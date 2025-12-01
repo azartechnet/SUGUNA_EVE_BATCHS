459c4559570c1f48a0f8109d0767e3ab5e54e5e2
azartechnet <azartechnet@gmail.com>
2025-12-01 19:48:44 +0530

    updated

--- Reactjs/project1/src/index.js
+++ Reactjs/project1/src/index.js
@@ -35,7 +35,7 @@ r1.render(React.createElement("div",null,myelem1,myelem2))*/
 
 //Login Form
 
-const Login=()=>{
+/*const Login=()=>{
   return(
     <>
       <form>
@@ -49,4 +49,112 @@ const Login=()=>{
   )
 }
 const r1=ReactDOM.createRoot(document.getElementById("root"))
-r1.render(<Login/>)
+r1.render(<Login/>)*/
+
+//React List
+
+/*const MyElem=()=>{
+  return(
+    <div>
+      <h1>MyList</h1>
+      <ul>
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+      </ul>
+      <ol>
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+      </ol>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<MyElem/>)*/
+
+//Conditional Rendering
+/*const x=20
+let text="";
+if(x>10)
+{
+  text="x is greater than 10"
+}
+else
+{
+  text="x is less than 10"
+}
+const MyElem=()=>{
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<MyElem/>)*/
+
+/*const x=10
+let text="";
+if(x>10)
+{
+  text="x is greater than 10"
+}
+else if(x==10)
+{
+  text="Equal to 10"
+}
+else
+{
+  text="x is less than 10"
+}
+const MyElem=()=>{
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<MyElem/>)*/
+
+//Task Nested if else statement
+
+/*import './index.css'
+const App=()=>{
+  return(
+    <>
+    <h1>Welcome</h1>
+    <p className="f1">This is Paragraph</p>
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//Function Component
+
+/*function Sample()
+{
+  return(
+    <div>
+      <h1>This is Function Component</h1>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//class Component
+
+class Sample extends React.Component
+{
+  render()
+  {
+    return(
+      <h1>Welcome</h1>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)
