9f7d450bc65cccd6c5579fe52530ef9fd25fc7da
azartechnet <azartechnet@gmail.com>
2026-01-02 19:39:44 +0530

    updated

--- Reactjs/routerapp1/src/pages/Layout.js
+++ Reactjs/routerapp1/src/pages/Layout.js
@@ -1,27 +1,62 @@
 import { Outlet,Link } from "react-router-dom";
-// import {Navbar,Nav,Container} from 'react-bootstrap'
-// import 'bootsrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Layout=()=>{
     return(
-        <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/product">Product</Link>
-                </li>
-            </ul>
-        </nav>
-        <Outlet/>
-        </>
+        <div>
+               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                  <Link className="navbar-brand" to="/">MySite</Link>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                  data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span> 
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                      <li className="nav-item">
+                          <Link className="nav-link" to="/">Home</Link>
+                      </li>
+                       <li className="nav-item">
+                          <Link className="nav-link" to="/contact">Contact</Link>
+                      </li>
+                       <li className="nav-item">
+                          <Link className="nav-link" to="/login">Login</Link>
+                      </li>
+                       <li className="nav-item">
+                          <Link className="nav-link" to="/product">Product</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+               </nav>
+               <Outlet/>
+        </div>
+
+
+
+
+
+
+
+        // <>
+        // <nav>
+        //     <ul>
+        //         <li>
+        //             <Link to="/">Home</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/contact">Contact</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/login">Login</Link>
+        //         </li>
+        //         <li>
+        //             <Link to="/product">Product</Link>
+        //         </li>
+        //     </ul>
+        // </nav>
+        // <Outlet/>
+        // </>
     )
 }
 export default Layout;
