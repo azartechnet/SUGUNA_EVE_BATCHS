a8e0d67b2a17030e529a3910a62e883e3cbccf2f
azartechnet <azartechnet@gmail.com>
2026-03-02 19:57:07 +0530

    updated

---
+++ Nodejs/nodep6.js
@@ -0,0 +1,28 @@
+//setTimeout using file
+var f1=require('fs')
+f1.writeFile('f1.txt','welcome to the file',function(err){
+    if(err)
+    {
+        console.log(err)
+    }
+    else
+    {
+        setTimeout(function(){
+            console.log("welcome to the Application")
+        },2000)
+        console.log("File Created...")
+    }
+})
+//Read the file after 3 seconds
+f1.readFile('f1.txt',function(err,data){
+    if(err)
+    {
+        console.log(err)
+    }
+    else
+    {
+        setTimeout(function(){
+            console.log(data.toString())
+        },3000)
+    }
+})
