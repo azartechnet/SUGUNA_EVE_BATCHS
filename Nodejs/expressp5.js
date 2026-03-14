78f893bdf1385644c233129ff64302bbc3c0b963
azartechnet <azartechnet@gmail.com>
2026-03-14 19:49:27 +0530

    updated

---
+++ Nodejs/expressp5.js
@@ -0,0 +1,7 @@
+const exp=require('express')
+const app=exp();
+const PORT=3000;
+app.get("/",(req,res)=>{
+    res.send(`<div><h2>Welcome to Express js</h2></div>`)
+}).listen(PORT)
+console.log("Server is Running....")
