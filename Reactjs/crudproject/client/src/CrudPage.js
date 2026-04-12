1f2175440b6491400ed5d49b928904217620d9d0
azartechnet <azartechnet@gmail.com>
2026-04-12 17:02:45 +0530

    updated

--- Reactjs/crudproject/client/src/CrudPage.js
+++ Reactjs/crudproject/client/src/CrudPage.js
@@ -6,7 +6,12 @@ function CrudPage()
 {
    const [foodName,setFoodName]=useState("");
    const [description,setDescription]=useState("");
+   const [foodList,setFoodList]=useState([])
+   const [newFoodName,setNewFoodName]=useState("")
 
+     useEffect(()=>{
+        fetchData();
+    },[])
   
    //inserted
    const addFoodData=()=>{
@@ -18,8 +23,22 @@ function CrudPage()
          console.log(err)
       })
    }
-
-   
+ //get data
+    const fetchData=()=>{
+        Axios.get("http://localhost:3001/read").then((response)=>{
+            console.log(response.data)
+            setFoodList(response.data)
+        })
+    }
+     //update
+    const updateFood=(id)=>{
+        Axios.put('http://localhost:3001/update',{id,newFoodName})
+        .then(()=>fetchData())
+    }
+        //delete
+    const deleteFood=(id)=>{
+        Axios.delete(`http://localhost:3001/delete/${id}`).then(()=>fetchData())
+        }
     return(
        <div className="container">
           <h1>CRUDPage</h1>
@@ -36,7 +55,30 @@ function CrudPage()
              onChange={(e)=>setDescription(e.target.value)}/>
           </div>
           <button className="btn btn-primary" onClick={addFoodData}>AddFood</button>
-         
+         <h3 className="mt-4">Get Data From Database</h3>
+        <table className="table table-bordered table-striped">
+            <thead className="table-dark">
+                <tr>
+                    <th>Food Name</th>
+                    <th>Food Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+             {foodList.map((val,key)=>(
+                <tr key={key}>
+                    <td>{val.foodName}</td>
+                    <td>{val.description}</td>
+
+                    <td>
+                        <input type="text" placeholder="UpdateFoodName" onChange={(e)=>setNewFoodName(e.target.value)}/>
+                         <button className="btn btn-primary" onClick={()=>updateFood(val._id)}>Edit</button></td>
+                      <td><button className="btn btn-danger" onClick={()=>deleteFood(val._id)}>Delete</button></td>
+                </tr>
+             ))}
+            </tbody>
+        </table>
        </div>
        
     )
--- Reactjs/crudproject/server/index.js
+++ Reactjs/crudproject/server/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 }   
 ));
 const FoodModel=require('./models/Food');
-mongoose.connect('mongodb://localhost:27017/food', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0.3fhvrpm.mongodb.net/?appName=Cluster0/food', {
 }).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
@@ -32,7 +32,51 @@ app.post('/insert', async (req, res) => {
     }
 });
 
+//Read the data
+app.get("/read",async(req,res)=>{
+    try{
+        const food=await FoodModel.find();
+        res.send(food)
+    }
+    catch(err)
+    {
+         res.send("Error")
+    }
+})
+//Updating the data
 
+app.put("/update",async(req,res)=>{
+    const {newFoodName,id}=req.body;
+    try{
+         const updateFood=await FoodModel.findById(id);
+         if(!updateFood)
+         {
+          return res.status(404).send("Data not found")
+         }
+         updateFood.foodName=newFoodName;
+         await updateFood.save()
+         res.send("Data updated successfully")
+      }
+      catch(err)
+      {
+        console.log(err)
+      }
+  })
+    //Deleting the data
+  app.delete("/delete/:id",async(req,res)=>{
+    const id = req.params.id; 
+    try{
+      const result=await FoodModel.findByIdAndDelete(id);
+      if(!result)
+      {
+         return res.status(404).send("Food items not found")
+      }
+      res.send("Food item deleted successfully")
+    }catch(err){
+          console.error(err);
+          res.status(500).send("Error deleting food item")
+      }
+    });
 app.listen(3001, () => {
     console.log('Server is running on port 3000');
 });
